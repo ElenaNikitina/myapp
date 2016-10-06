f27d525cd66c2c3ad44db3b553767f220ab22917
Elena Nikitina <elena.nikitina@orangesputnik.eu>
2016-10-06 17:57:12 +0300

    some changes in database

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,9 +1,10 @@
 'use strict';
 
-var myApp = angular.module('myApp', ['ngRoute', 'ngSQLite']);
+var myApp = angular.module('myApp', ['firebase', 'ngRoute']);
 
-myApp.config(['$routeProvider', '$locationProvider', function($routeProvide, $locationProvider){
-	$routeProvide
+
+myApp.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider){
+	$routeProvider
 		.when('/',{
 			templateUrl:'template/race.html',
 			controller: 'RaceCtrl'
@@ -25,10 +26,9 @@ myApp.config(['$routeProvider', '$locationProvider', function($routeProvide, $lo
         });
 }]);
 
-myApp.controller('RaceCtrl',['$scope', '$rootScope', '$http', '$location', 
+myApp.controller('RaceCtrl',['$scope', '$rootScope', '$http', '$location',
 function($scope, $rootScope, $http, $location){
-	console.log('$location.url() - ', $location.url());
-	$rootScope.params = {};
+//	$rootScope.params.path = '';
 		
 }]);
 
@@ -36,16 +36,20 @@ myApp.controller('GtopenCtrl',['$scope', '$rootScope', '$http', '$location',
 function($scope, $rootScope, $http, $location){
 	$http.get('pilots/pilotsinfo.json').success(function(data){
 		$scope.pilots = data;
-		$rootScope.params.path = 'http://www.chayka-ring.com/upload/file/Koltco_2016/6_dmitriy_zhayvoronok_andrey_evtushenko.jpg';
+//		$rootScope.params.path = 'http://www.chayka-ring.com/upload/file/Koltco_2016/6_dmitriy_zhayvoronok_andrey_evtushenko.jpg';
 	});
 }]);
 
-myApp.controller('Y1600Ctrl',['$scope', '$rootScope', '$http', '$location',
- function($scope, $rootScope, $http, $location){
-	$rootScope.params.path = 'http://www.chayka-ring.com/upload/file/Koltco_2016/10_valeriy_shevelev.jpg';
+myApp.controller('Y1600Ctrl',['$scope', '$rootScope', '$http', '$location', '$firebaseArray', 
+ function($scope, $rootScope, $http, $location, $firebaseArray){
+//	$rootScope.params.path = 'http://www.chayka-ring.com/upload/file/Koltco_2016/10_valeriy_shevelev.jpg';
+	var ref = new Firebase("https://myapp-73570.firebaseio.com/");
+	$scope.myData = $firebaseArray(ref);
+	console.log($scope.myData)
+
 }]);
 
 myApp.controller('GccupCtrl',['$scope', '$rootScope', '$http', '$location',
  function($scope, $rootScope, $http, $location){
-	$rootScope.params.path = 'http://www.chayka-ring.com/upload/file/Koltco_2016/6244.jpg';
+//	$rootScope.params.path = 'http://www.chayka-ring.com/upload/file/Koltco_2016/6244.jpg';
 }]);
